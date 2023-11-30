e47fdb5f55f6f63586b9900402eecd127dbebb30
Bharat <bs987143@gmail.com>
2023-11-30 13:48:20 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,31 @@ import Html5QrcodePlugin from './Html5QrcodePlugin.jsx';
 import ResultContainerPlugin from './ResultContainerPlugin.jsx';
 
 const App = (props) => {
-    const [decodedResults, setDecodedResults] = useState([]);
-    const onNewScanResult = (decodedText, decodedResult) => {
-        console.log("App [result]", decodedResult);
-        setDecodedResults(prev => [...prev, decodedResult]);
-    };
+  const [decodedResults, setDecodedResults] = useState([]);
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log('App [result]', decodedResult);
+    setDecodedResults((prev) => [...prev, decodedResult]);
+  };
 
-    return (
-        <div className="App">
-            <section className="App-section">
-                <div className="App-section-title"> Html5-qrcode React demo</div>
-                <br />
-                <br />
-                <br />
-                <Html5QrcodePlugin
-                    fps={10}
-                    qrbox={250}
-                    disableFlip={false}
-                    qrCodeSuccessCallback={onNewScanResult}
-                />
-                <ResultContainerPlugin results={decodedResults} />
-                <HowToUse />
-            </section>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <section className='App-section'>
+        <div className='App-section-title'> Html5-qrcode React demo</div>
+        <br />
+        <br />
+        <br />
+        <Html5QrcodePlugin
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          qrCodeSuccessCallback={onNewScanResult}
+          aspectRatio={1}
+        />
+        <ResultContainerPlugin results={decodedResults} />
+        <HowToUse />
+      </section>
+    </div>
+  );
 };
 
 export default App;
